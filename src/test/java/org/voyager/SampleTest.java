ec001ef40f1de56901ba39ebeb454c7ffec358f1
aarahman <aarahman@tekion.com>
2024-12-08 23:10:48 +0530

    Added test cases for NetworkLogUtils

--- src/test/java/org/voyager/SampleTest.java
+++ src/test/java/org/voyager/SampleTest.java
@@ -12,6 +12,7 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class SampleTest {
@@ -19,7 +20,39 @@ public class SampleTest {
     static final int timeoutInSeconds = 10;
 
     public static void main(String...arg) throws MockerException {
+        networkLogMocking();
         testMocking();
+    }
+
+    private static void networkLogMocking() {
+        driver = new ChromeDriver();
+        NetworkLogUtils networkLogUtils = new NetworkLogUtils(driver);
+        try {
+            driver.get("https://www.kia.com/in/buy/find-a-dealer.html");
+            waitForClickable(By.cssSelector("#select-state-button")).ifPresent(WebElement::click);
+            waitForClickables(By.cssSelector("li[class*='ui-menu-item']"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            Set<NetworkLog> networkLogs = networkLogUtils
+                    .getNetworkLogMap()
+                    .values()
+                    .stream()
+                    .filter(networkLog -> networkLog.getApiUrl().contains("https://www.kia.com/api"))
+                    .collect(Collectors.toSet());
+            System.out.println(networkLogUtils.getNetworkLogMap().size());
+            System.out.println(networkLogs
+                    .stream()
+                    .map(networkLog -> String.format("ApiUrl : %s,\nApiMethod : %s,\nRequestId : %s,\nStartTimeStamp : %s,\nUiUrl : %s,\n" +
+                                    "EndTimeStamp : %s,\nDownloadedData : %s,\nRequestHeader : %s,\nResponseHeader : %s,\n" +
+                                    "ResponseBody : %s,\n\n",
+                            networkLog.getApiUrl(), networkLog.getApiMethod(), networkLog.getRequestId(),
+                            networkLog.getStartTimeStamp(), networkLog.getUiUrl(), networkLog.getEndTimeStamp(), networkLog.getDownloadedData(),
+                            networkLog.getRequestHeader(), networkLog.getResponseHeader(), networkLog.getResponseBody()))
+                    .collect(Collectors.joining(",\n")));
+            networkLogUtils.stopNetworkLog();
+            driver.quit();
+        }
     }
 
     private static void testMocking() throws MockerException {
